f6908c969ffa651dd6b7a41ca3b46c307138d394
Nick Siri <nicolassirimongkol@gmail.com>
2024-06-23 22:12:16 -0700

    chore: update README.md

--- README.md
+++ README.md
@@ -5,11 +5,13 @@
 This project contains a suite of smoke tests for the [QR Code Monkey](https://www.qrcode-monkey.com) website. These tests are written using [Playwright](https://playwright.dev/) and TypeScript, designed to ensure the main functionality of the site is working.
 
 ## Challenges
+
 The website is rich in terms of selector attributes so I found no problems in dealing with the selectors. However, after testing and writing code I ran out of QR generations for the day. This put a huge pause in my development work as I'll have to wait till the next day in order to keep testing.
 
 Another fun challenge was getting the QR code parsed, this was fun to do as I had to use very specific types for the library I was using but Playwright does have a screenshot function so that was very helpful.
 
 ## Design Choices
+
 I went with Playwright because I have been using it for the past 3 years and I really enjoy the documentation and minimal set up. I decided to test 3 different browsers in headless mode for speed.
 
 Typescript is the language I wrote this project, I hope that is okay!
@@ -20,12 +22,15 @@ The test suite covers the following features of the QR Code Monkey website:
 
 1. **Homepage Load**: Verifies that the homepage loads correctly.
 2. **Dynamic Tabs**: Verifies that the content on each tab is visible, will get all tabs and do a quick test for each one.
-3. **QR Code Generation for Webpage QR**: Ensures that QR codes are generated and takes a screenshot of the QR code, then decodes the QR. 
-4. ***QR Code Generation for Downloaded QR**: Ensures that QR codes are generated and decodes the QR in png. 
+3. **Display Error Message for URL Input**
+4. **Adds a logo to the QR**
+5. **QR Code Generation for Webpage QR**: Ensures that QR codes are generated and takes a screenshot of the QR code, then decodes the QR.
+6. **\*QR Code Generation for Downloaded QR**: Ensures that QR codes are generated and decodes the QR in png.
 
 ## Future Plans
-- Add more tests for QR color change, error handling in UI, and other QR code types
-- Test more accesibility as I noticed the page does have selectors that can be tested
+
+- Add more tests for QR color change, setting other basic features, and other QR code types
+- Test more accessibility as I noticed the page does have selectors that can be tested
 - Add linting and prettier to code base for better format on save and CI
 
 ## How to Run the Tests
@@ -39,7 +44,7 @@ The test suite covers the following features of the QR Code Monkey website:
 
 1. Clone the repository:
    ```
-   git clone https://github.com/yourusername/qrcode-monkey-smoke-tests.git
+   git clone https://github.com/nsirimo/playwright-qr.git
    cd qrcode-monkey-smoke-tests
    ```
 2. Install the dependencies:
@@ -48,8 +53,9 @@ The test suite covers the following features of the QR Code Monkey website:
    ```
 
 ### Running the Tests
-To execute the tests, run the following command:
-  ```
-  npx playwright test
-  ```
 
+To execute the tests, run the following command:
+
+```
+npx playwright test
+```
--- tests/smoke.spec.ts
+++ tests/smoke.spec.ts
@@ -1,67 +1,74 @@
-import { test, expect, Page } from '@playwright/test';
-import { PNG } from 'pngjs';
-import decodeQR from 'jsqr';
-import fs from 'fs';
+import { test, expect, Page } from "@playwright/test";
+import { PNG } from "pngjs";
+import decodeQR from "jsqr";
+import fs from "fs";
 
-test.describe('QR Code Monkey Smoke Tests', () => {
-  const inputUrl = 'https://www.nickAtBitly.com';
+test.describe("QR Code Monkey Smoke Tests", () => {
+  const inputUrl = "https://qrstud.io/qrmnky";
 
   test.beforeEach(async ({ page }) => {
     // Cookie Consent bypass in case it shows up
-    await page.goto('/');
-    const cookieConsentButton = page.locator('text=Accept');
+    await page.goto("/");
+    const cookieConsentButton = page.locator("text=Accept");
     if (await cookieConsentButton.isVisible()) {
       await cookieConsentButton.click();
     }
   });
 
   async function createQRCode(page: Page): Promise<void> {
-    await page.fill('#qrcodeUrl', inputUrl);
-    await page.click('#button-create-qr-code');
+    await page.fill("#qrcodeUrl", inputUrl);
+    await page.click("#button-create-qr-code");
   }
 
-  test.describe('QR Generator Tests', () => {
-    test('should generate a QR Code with default settings', async ({ page }) => {
-      await expect(page.locator('img[src="/img/default-preview-qr.svg"]')).toBeVisible({ timeout: 10000 });
+  test.describe("QR Generator Tests", () => {
+    test("should generate a QR Code with default settings", async ({
+      page,
+    }) => {
+      await expect(
+        page.locator('img[src="/img/default-preview-qr.svg"]'),
+      ).toBeVisible({ timeout: 10000 });
 
       createQRCode(page);
 
-      await expect(page.locator('img[src*="api.qrcode-monkey.com/tmp/"]')).toBeVisible({ timeout: 10000 });
+      await expect(
+        page.locator('img[src*="api.qrcode-monkey.com/tmp/"]'),
+      ).toBeVisible({ timeout: 60000 });
     });
 
-    test('should generate and validate a QR Code', async ({ page }) => {
-      await page.fill('#qrcodeUrl', inputUrl);
-      await page.click('#button-create-qr-code');
-
+    test("should generate and validate a QR Code", async ({ page }) => {
       createQRCode(page);
-      await expect(page.locator('img[src*="api.qrcode-monkey.com/tmp/"]')).toBeVisible({ timeout: 10000 });
+      await expect(
+        page.locator('img[src*="api.qrcode-monkey.com/tmp/"]'),
+      ).toBeVisible({ timeout: 60000 });
 
-      await page.click('#button-download-qr-code-png')
-      const [download] = await Promise.all([
-        page.waitForEvent('download')
-      ]);
+      await page.click("#button-download-qr-code-png");
+      const [download] = await Promise.all([page.waitForEvent("download")]);
 
       const qrDownloadPath = await download.path();
       expect(qrDownloadPath).not.toBeNull();
       const buffer = fs.readFileSync(qrDownloadPath);
 
       const qrPng = PNG.sync.read(buffer);
-      const imageData = new Uint8ClampedArray(qrPng.data)
+      const imageData = new Uint8ClampedArray(qrPng.data);
 
       const qrCode = decodeQR(imageData, qrPng.width, qrPng.height);
       expect(qrCode?.data).toBe(inputUrl);
       fs.unlinkSync(qrDownloadPath);
     });
 
-    test('should take screenshot of QR code and validate', async ({ page }) => {
+    test("should take screenshot of QR code and validate", async ({ page }) => {
       createQRCode(page);
-      await expect(page.locator('img[src*="api.qrcode-monkey.com/tmp/"]')).toBeVisible({ timeout: 10000 });
+      await expect(
+        page.locator('img[src*="api.qrcode-monkey.com/tmp/"]'),
+      ).toBeVisible({ timeout: 10000 });
 
       // Take screenshot of the QR code on the webpage
-      const screenshot = await page.screenshot({
-        type: 'png',
-        fullPage: true
-      });
+      const screenshot = await page
+        .locator('img[src*="api.qrcode-monkey.com/tmp/"]')
+        .screenshot({
+          type: "png",
+        });
+
       const qrPng = PNG.sync.read(screenshot);
 
       // Decode the QR code from the screenshot
@@ -71,19 +78,33 @@ test.describe('QR Code Monkey Smoke Tests', () => {
       if (qrCode) {
         expect(qrCode.data).toBe(inputUrl);
       } else {
-        throw new Error('Failed to decode QR code from screenshot')
+        throw new Error("Failed to decode QR code from screenshot");
       }
+    });
+
+    test("should add a logo to the QR code", async ({ page }) => {
+      const logoAccordion = page.locator(
+        "div[ng-class*=\"editView==='logo'\"]",
+      );
+      await logoAccordion.click();
+
+      const firstLogo = page.locator(".shape-options .shape.ng-scope").first();
+      await expect(firstLogo).toBeVisible();
+      createQRCode(page);
+      await expect(
+        page.locator('img[src*="api.qrcode-monkey.com/tmp/"]'),
+      ).toBeVisible({ timeout: 60000 });
     });
   });
 
-  test.describe('UI Tests', () => {
+  test.describe("UI Tests", () => {
     async function fetchTabIds(page: Page): Promise<string[]> {
       // Evaluate JavaScript on the page to fetch tab IDs
       const tabIds: (string | null)[] = await page.evaluate(() => {
-        const tabElements = document.querySelectorAll('a.tab');
+        const tabElements = document.querySelectorAll("a.tab");
         const ids = Array.from(tabElements).map((el) => {
           // Extract the tab ID from the ng-click attribute
-          const ngClick = el.getAttribute('ng-click');
+          const ngClick = el.getAttribute("ng-click");
           if (ngClick) {
             const match = /setTab\('(.*)'\)/.exec(ngClick);
             if (match && match[1]) {
@@ -96,15 +117,15 @@ test.describe('QR Code Monkey Smoke Tests', () => {
       });
 
       // Filter out null values and convert to string array
-      const filteredTabIds = tabIds.filter(id => id !== null) as string[];
+      const filteredTabIds = tabIds.filter((id) => id !== null) as string[];
       return filteredTabIds;
     }
 
-    test.only('should have expected homepage title', async ({ page }) => {
+    test("should have expected homepage title", async ({ page }) => {
       await expect(page).toHaveTitle(/QR.*Code.*Monkey/);
     });
 
-    test.only('should navigate through each tab visibility', async ({ page }) => {
+    test("should navigate through each tab visibility", async ({ page }) => {
       // Fetch tab IDs dynamically from the website
       const tabIds: string[] = await fetchTabIds(page);
 
@@ -113,12 +134,29 @@ test.describe('QR Code Monkey Smoke Tests', () => {
         const tabSelector = `a[ng-click="setTab('${tabId.replace(/'/g, "\\'")}')"]`;
         await page.click(tabSelector);
 
-        await page.waitForSelector('.settings', { state: 'visible' });
-        const pageContent = await page.$('.settings');
+        await page.waitForSelector(".settings", { state: "visible" });
+        const pageContent = await page.$(".settings");
         expect(pageContent).toBeTruthy();
       }
     });
+
+    test("should display an error message for invalid url input", async ({
+      page,
+    }) => {
+      const createButton = page.locator("button", {
+        hasText: "Create QR Code",
+      });
+      await createButton.click();
+
+      await page.fill("#qrcodeUrl", "not-a-url");
+      await page.click("#button-create-qr-code");
+
+      // Error above generate QR button
+      const generateBtnErrMsg = page.locator(".alert-danger");
+      await expect(generateBtnErrMsg).toBeVisible();
+      await expect(generateBtnErrMsg).toHaveText(
+        /There are errors you have to fix before generating/,
+      );
+    });
   });
 });
-
-
